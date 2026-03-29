2368c460694f1c7d9d609455b6f476ceb48380a1
laura <lalakinha2003@gmail.com>
2026-03-28 23:25:29 -0300

    loop

--- main.c
+++ main.c
@@ -10,90 +10,54 @@ int main(){
     
     srand(42); 
 
-    printf("=== TESTE ===\n");
+    printf("TESTE \n");
     
     Dados d = lerDados("input.dat");
 
-    for(int g = 0; g < d.geracoes; g++){
-
-        
-    }
-
-    Individuo *populacao = gerarPopulacao( d.tamanhoPop);
-
+    Individuo *populacao = gerarPopulacao(d.tamanhoPop);
     preencherPopulacao(populacao, d.tamanhoPop, -10, 10);
 
-    //float melhorFitness = 0;
-    //int melhorIndividuo = 0;
+    //loop
+    for(int g = 0; g < d.geracoes; g++){
 
-    for(int i = 0; i < d.tamanhoPop; i++){
+        printf("\n GERACAO %d \n", g);
 
-        float erro = calcularErro(populacao[i], d.qtdPontos, d.valorX, d.valorY);
+        
+        ordenarPopulacao(populacao, d.tamanhoPop, d.qtdPontos, d.valorX, d.valorY);
 
-        float fitness = calcularFitness(populacao[i], d.qtdPontos, d.valorX, d.valorY);
+        
+        Individuo melhor = populacao[0];
 
-        printf("Individuo %d -> a = %.2f \t| b = %.2f \t| erro = %.2f\t\t| fitness = %.4f\n\n",
-         i,
-         populacao[i].a, 
-         populacao[i].b,
-         erro,
-         fitness);
+        float melhorFitness = calcularFitness(melhor, d.qtdPontos, d.valorX, d.valorY);
+        float melhorErro = calcularErro(melhor, d.qtdPontos, d.valorX, d.valorY);
+
+        printf("Melhor -> a = %.2f | b = %.2f | fitness = %.4f | erro = %.2f\n",
+               melhor.a, melhor.b, melhorFitness, melhorErro);
+
+        
+        salvarResultados("output.dat", melhor, melhorFitness, melhorErro);
+
+        
+        Individuo mutado = melhor;
+        mutacao(&mutado);
+
+        printf("Mutado -> a = %.2f | b = %.2f\n", mutado.a, mutado.b);
+
+        
+        populacao[d.tamanhoPop - 1] = mutado;
     }
 
-    ordenarPopulacao(populacao, d.tamanhoPop, d.qtdPontos, d.valorX, d.valorY);
-
-    printf("\nPOPULACAO ORDENADA DE ACORDO COM O FITNESS\n");
-    for(int i =0; i < d.tamanhoPop; i++){
-         float fitness = calcularFitness(populacao[i], d.qtdPontos, d.valorX, d.valorY);
-
-         printf("Individuo %d -> a = %.2f | b = %.2f | fitness = %.4f\n",
-        i, populacao[i].a, populacao[i].b, fitness);
-    }
-
-    Individuo melhor = populacao[0];
-
-    float melhorFitness = calcularFitness(melhor, d.qtdPontos, d.valorX, d.valorY);
-    float melhorErro = calcularErro(melhor, d.qtdPontos, d.valorX, d.valorY);
-
-    printf("MELHOR INDIVIDUO\n");
-    printf("a = %.2f | b = %.2f | fitness = %.4f | erro = %.2f\n",
-           melhor.a, melhor.b, melhorFitness, melhorErro);
-
-           salvarResultados("output.dat", melhor, melhorFitness, melhorErro);
-
-           //crossover
-    Individuo pai1 = populacao[0];
-    Individuo pai2 = populacao[1];
-    
-    Individuo filho = crossover(pai1, pai2);
-
-    printf("FILHO QUE VAI GERAR\n");
-    printf("a = %.2f | b = %.2f\n", filho.a, filho.b);
-
-
-    printf("DIVISAO DE MELHORES E PIORES\n");
-
-    printf("Melhores:\n");
-    int metade = d.tamanhoPop / 2;
-    for(int i = 0; i < metade; i++){
-        printf("Ind %d\n", i);
-    }
-
-    printf("Piores:\n");
-    for(int i = metade; i < d.tamanhoPop; i++){
-        printf("Ind %d\n", i);
-    }
-
-
-    //TESTE
+    // TESTE 
+    printf("\nDADOS\n");
     printf("numero de pontos: %d\n", d.qtdPontos);
     printf("populacao: %d\n", d.tamanhoPop);
     printf("geracoes: %d\n", d.geracoes);
 
-    for(int i = 0; i < d.qtdPontos; i++){
+    for(int i = 0; i< d.qtdPontos; i++){
         printf("X = %.2f | Y = %2f\n", d.valorX[i], d.valorY[i]);
     }
 
+    // liberar memória
     liberarPopulacao(populacao);
     free(d.valorX);
     free(d.valorY);
